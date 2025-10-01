bac2f479b6812b77a90342a3e295011ce72b9dd0
tuniekov <tuniekov@gmail.com>
2025-10-01 23:46:30 +0700

    1

--- _build/configs/resources.js
+++ _build/configs/resources.js
@@ -1,13 +1,16 @@
 export default {
     // 'web': {
     //     'catalog': {
-    //         'pagetitle': 'Каталог',
-    //         'hidemenu': false,
+    //         'pagetitle': 'Каталог', //Название страницы
+    //         'hidemenu': false, // скрыть из меню
     //         'published': true,
     //         'isfolder': true,
-    //         'class_key': 'gsCategory',
+    //         'update': true, // Если ресурс существует обновлять его при установке пакета
+    //         'config_name': 'gtsshoplk_p_catalog', // имя системной настройки в которую записывается id ресурса
+    //         'groups': 'users', // Группа ресурсов
+    //         'class_key': 'gsCategory', //Класс ресурса
     //         'properties': {
-    //             'templatename': 'CatalogTemplate',
+    //             'templatename': 'CatalogTemplate', //Шаблон страницы
     //         },
     //     },
     //     // Добавьте здесь другие ресурсы по необходимости
